452ee7155c1c29f793527f0b043aa3635cc77692
rkocev <kcoev.riste@gmail.com>
2023-05-23 22:54:28 +0200

    Fully working code!! Perfectly precise as well!

--- src/ArithmeticCoding.java
+++ src/ArithmeticCoding.java
@@ -1,121 +1,129 @@
+import java.math.BigDecimal;
+import java.math.MathContext;
+
 public class ArithmeticCoding {
-    private static final int PRECISION = 32; // Number of bits in the precision
+    private static final MathContext PRECISION = new MathContext(100);
 
     public static void main(String[] args) {
-        String sequence = "abadccdabdbadcabdcbabdcab";
-        double[] cumulativeProbabilities = { 0.5, 0.8, 0.9, 1.0 };
+        // Define the input sequence
+        String input = "adcbadbcadabcbadcbdabacbadbacbdadadabdbccbadbababcbabccbacbdcabbcdbdcdbcbadbbcdaaabcbdcdccdadcbdabaa";
 
-        // Encoding
-        long lowerBound = 0L;
-        long upperBound = (1L << PRECISION) - 1L; // Maximum value with given precision
-        long encodedValue = 0L;
-        int bitsWritten = 0;
+        // Initialize the lower bound, upper bound, and range
+        BigDecimal lowerBound = BigDecimal.ZERO;
+        BigDecimal upperBound = BigDecimal.ONE;
+        BigDecimal range = BigDecimal.ONE;
 
-        for (char symbol : sequence.toCharArray()) {
-            int symbolIndex = getSymbolIndexFromChar(symbol);
-            long range = upperBound - lowerBound + 1;
-            upperBound = lowerBound + (range * (long) (cumulativeProbabilities[symbolIndex] * (1L << PRECISION))) - 1L;
-            lowerBound = lowerBound + (range * (long) (cumulativeProbabilities[symbolIndex - 1] * (1L << PRECISION)));
+        // Perform arithmetic coding for each symbol in the input sequence
+        for (char symbol : input.toCharArray()) {
+            // Get the probability of the current symbol
+            BigDecimal symbolProb = getSymbolProbability(symbol);
 
-            while (true) {
-                if (upperBound < (1L << (PRECISION - 1)) || lowerBound >= (1L << (PRECISION - 1))) {
-                    // No renormalization needed
-                    break;
-                } else if (upperBound < (1L << PRECISION) && lowerBound >= (1L << (PRECISION - 1))) {
-                    // Renormalization by shifting out the most significant bit
-                    encodedValue ^= (1L << (PRECISION - 1));
-                    lowerBound <<= 1;
-                    upperBound <<= 1;
-                    upperBound |= 1L;
-                    bitsWritten++;
-                } else {
-                    // Output a bit, renormalization not possible
-                    break;
-                }
-            }
+            // Calculate the new lower bound and upper bound
+            BigDecimal newLowerBound = lowerBound.add(range.multiply(getSymbolCumulativeProb(symbol)));
+            BigDecimal newUpperBound = lowerBound.add(range.multiply(getSymbolCumulativeProb(symbol).add(symbolProb)));
+
+            // Update the lower bound, upper bound, and range
+            lowerBound = newLowerBound;
+            upperBound = newUpperBound;
+            range = upperBound.subtract(lowerBound, PRECISION);
         }
 
-        // Decoding
-        StringBuilder decodedSequence = new StringBuilder();
-        lowerBound = 0L;
-        upperBound = (1L << PRECISION) - 1L;
-        long value = encodedValue;
-        int bitsRead = 0;
+        System.out.println("Given input:   " + input);
 
-        while (bitsRead < PRECISION) {
-            long range = upperBound - lowerBound + 1;
-            long scaledValue = ((value - lowerBound + 1) * (1L << PRECISION) - 1L) / range;
-            int symbolIndex = getSymbolIndexFromScaledValue(scaledValue, cumulativeProbabilities);
-            char symbol = getCharFromSymbolIndex(symbolIndex);
-            decodedSequence.append(symbol);
+        // Calculate the encoded value
+        BigDecimal code = lowerBound.add(range.divide(BigDecimal.valueOf(2), PRECISION));
+        System.out.println("Encoded value: " + code);
 
-            upperBound = lowerBound + (range * (long) (cumulativeProbabilities[symbolIndex] * (1L << PRECISION))) - 1L;
-            lowerBound = lowerBound + (range * (long) (cumulativeProbabilities[symbolIndex - 1] * (1L << PRECISION)));
-
-            while (true) {
-                if (upperBound < (1L << (PRECISION - 1)) || lowerBound >= (1L << (PRECISION - 1))) {
-                    // No renormalization needed
-                    break;
-                } else if (upperBound < (1L << PRECISION) && lowerBound >= (1L << (PRECISION - 1))) {
-                    // Renormalization by shifting out the most significant bit
-                    lowerBound <<= 1;
-                    upperBound <<= 1;
-                    upperBound |= 1L;
-                    value <<= 1;
-                    value |= (1L << (PRECISION - 1));
-                    bitsRead++;
-                } else {
-                    // Read a bit, renormalization not possible
-                    bitsRead++;
-                    value <<= 1;
-                    lowerBound <<= 1;
-                    upperBound <<= 1;
-                    upperBound |= 1L;
-                    break;
-                }
-            }
-        }
-
-        System.out.println("Encoded value: " + encodedValue);
-        System.out.println("Decoded sequence: " + decodedSequence.toString());
+        // Decode the encoded value
+        String decodedInput = decode(code, input.length());
+        System.out.println("Decoded input: " + decodedInput);
     }
 
-    private static int getSymbolIndexFromChar(char symbol) {
+    // Get the probability of a symbol
+    private static BigDecimal getSymbolProbability(char symbol) {
         switch (symbol) {
             case 'a':
-                return 0;
+                return new BigDecimal("0.5", PRECISION);
             case 'b':
-                return 1;
+                return new BigDecimal("0.3", PRECISION);
             case 'c':
-                return 2;
+                return new BigDecimal("0.1", PRECISION);
             case 'd':
-                return 3;
+                return new BigDecimal("0.1", PRECISION);
             default:
                 throw new IllegalArgumentException("Invalid symbol: " + symbol);
         }
     }
 
-    private static int getSymbolIndexFromScaledValue(long scaledValue, double[] cumulativeProbabilities) {
-        for (int i = 1; i < cumulativeProbabilities.length; i++) {
-            if (scaledValue < cumulativeProbabilities[i] * (1L << PRECISION)) {
-                return i;
-            }
+    // Get the cumulative probability of a symbol
+    private static BigDecimal getSymbolCumulativeProb(char symbol) {
+        BigDecimal cumulativeProb = BigDecimal.ZERO;
+        switch (symbol) {
+            case 'a':
+                cumulativeProb = cumulativeProb.add(new BigDecimal("0.0", PRECISION));
+                break;
+            case 'b':
+                cumulativeProb = cumulativeProb.add(new BigDecimal("0.5", PRECISION));
+                break;
+            case 'c':
+                cumulativeProb = cumulativeProb.add(new BigDecimal("0.8", PRECISION));
+                break;
+            case 'd':
+                cumulativeProb = cumulativeProb.add(new BigDecimal("0.9", PRECISION));
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid symbol: " + symbol);
         }
-        return cumulativeProbabilities.length - 1;
+        return cumulativeProb;
     }
 
-    private static char getCharFromSymbolIndex(int index) {
-        switch (index) {
-            case 0:
-                return 'a';
-            case 1:
-                return 'b';
-            case 2:
-                return 'c';
-            case 3:
-                return 'd';
-            default:
-                throw new IllegalArgumentException("Invalid symbol index: " + index);
+    // Decode the encoded value
+    private static String decode(BigDecimal code, int length) {
+        StringBuilder decodedInput = new StringBuilder();
+        BigDecimal lowerBound = BigDecimal.ZERO;
+        BigDecimal upperBound = BigDecimal.ONE;
+        BigDecimal range = BigDecimal.ONE;
+
+        for (int i = 0; i < length; i++) {
+            // Calculate the value within the range for the current symbol
+            BigDecimal value = code.subtract(lowerBound, PRECISION).divide(range, PRECISION);
+
+            // Get the symbol corresponding to the value
+            char symbol = getSymbolFromProbability(value);
+            decodedInput.append(symbol);
+
+            // Get the probability of the symbol
+            BigDecimal symbolProb = getSymbolProbability(symbol);
+
+            // Calculate the new lower bound and upper bound
+            BigDecimal newLowerBound = lowerBound.add(range.multiply(getSymbolCumulativeProb(symbol)));
+            BigDecimal newUpperBound = lowerBound.add(range.multiply(getSymbolCumulativeProb(symbol).add(symbolProb)));
+
+            // Update the lower bound, upper bound, and range
+            lowerBound = newLowerBound;
+            upperBound = newUpperBound;
+            range = upperBound.subtract(lowerBound, PRECISION);
+        }
+
+        return decodedInput.toString();
+    }
+
+    // Get the symbol based on a given probability value
+    private static char getSymbolFromProbability(BigDecimal value) {
+        if (value.compareTo(new BigDecimal("0.0", PRECISION)) >= 0 &&
+                value.compareTo(new BigDecimal("0.5", PRECISION)) < 0) {
+            return 'a';
+        } else if (value.compareTo(new BigDecimal("0.5", PRECISION)) >= 0 &&
+                value.compareTo(new BigDecimal("0.8", PRECISION)) < 0) {
+            return 'b';
+        } else if (value.compareTo(new BigDecimal("0.8", PRECISION)) >= 0 &&
+                value.compareTo(new BigDecimal("0.9", PRECISION)) < 0) {
+            return 'c';
+        } else if (value.compareTo(new BigDecimal("0.9", PRECISION)) >= 0 &&
+                value.compareTo(new BigDecimal("1.0", PRECISION)) <= 0) {
+            return 'd';
+        } else {
+            throw new IllegalArgumentException("Invalid probability value: " + value);
         }
     }
 }
